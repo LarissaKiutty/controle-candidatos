60c6a1d71d9d0018efdfed2bf111e8c4a20e93f0
Larissa Kiutty <larissa.souza1941@gmail.com>
2024-07-17 23:17:58 -0300

    FEAT: starting a candidate management project during a recruitment process.

---
+++ controle-candidatos/src/ProcessoSeletivo.java
@@ -0,0 +1,20 @@
+public class ProcessoSeletivo {
+    public static void main(String[] args) throws Exception {
+        analisarCandidato(1900.0);
+        analisarCandidato(2200.0);
+        analisarCandidato(2000.0);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.00;
+
+        if(salarioBase > salarioPretendido) {
+            System.out.println("Ligar para o candidato");
+        } else if(salarioBase == salarioPretendido) {
+            System.out.println("Ligar para o candidato com contra proposta");
+        } else {
+            System.out.println("Aguardando o resultado dos demais candidatos");
+        }
+        
+    }
+}
