56f9b19835dcdf6ab45dd10fe849d100c4cc2828
Larissa Kiutty <larissa.souza1941@gmail.com>
2024-07-19 13:51:51 -0300

    FEAT: printing a list of selected candidates

--- controle-candidatos/src/ProcessoSeletivo.java
+++ controle-candidatos/src/ProcessoSeletivo.java
@@ -1,8 +1,32 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos() {
+        String[] candidatos = {"Pedro", "Chris", "Will", "Alicia", "Kiutty", "Bunibu", "Migueru", "Gabriel", "Mateus", "João"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+       
+        while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+            System.out.println("O candidato, " + candidato + " solicitou este valor de salário " + salarioPretendido);
+
+            if(salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionando para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 
     static void analisarCandidato(double salarioPretendido) {
