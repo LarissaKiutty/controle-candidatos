e8cfb9198fd655e0edf42a69bab91853f1cb6d07
Larissa Kiutty <larissa.souza1941@gmail.com>
2024-07-22 18:45:47 -0300

    FEAT: creating a function to make calls to the candidates, following business rules.

--- controle-candidatos/src/ProcessoSeletivo.java
+++ controle-candidatos/src/ProcessoSeletivo.java
@@ -1,8 +1,41 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        selecaoCandidatos();
+        String[] candidatos = {"Pedro", "Chris", "Will", "Alicia", "Kiutty", "Bunibu", "Migueru", "Gabriel", "Mateus", "João"};
+
+        for(String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
+    }
+
+   static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            
+            if(continuarTentando) {
+                tentativasRealizadas++;
+            } else {
+                System.out.println("Contato realizado com sucesso!");
+            }
+
+        } while(continuarTentando && tentativasRealizadas < 3);
+
+        if(atendeu) {
+            System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " tentativa.");
+        } else {
+            System.out.println("Não conseguimos contato com " + candidato + ", número máximo de tentativas: " + tentativasRealizadas);
+        }
+   }
+   
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
     }
 
     static void selecaoCandidatos() {
